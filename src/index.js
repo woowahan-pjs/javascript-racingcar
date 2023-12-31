319923c47bb38dd1a1d5d1fd8d6ce8b36c709b86
woowahan-pjs <jason@woowahan.com>
2023-12-31 16:15:08 +0900

    feat: implement race function

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ const rl = readline.createInterface({ input, output });
     "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n",
     (answer) => {
       const cars = createCars(answer);
-      console.log(cars.map((it) => it.name));
-      askAttemptCount();
+      askAttemptCount(cars);
     }
   );
 })();
@@ -20,10 +19,32 @@ function createCars(answer) {
   return answer.split(",").map((it) => new Car(it.trim()));
 }
 
-function askAttemptCount() {
+function askAttemptCount(cars) {
   rl.question("시도할 횟수는 몇 회인가요?\n", (answer) => {
     const attemptCount = new AttemptCount(answer);
-    console.log(attemptCount.value);
+    race(cars, attemptCount);
     rl.close();
   });
+}
+
+function race(cars, attemptCount) {
+  console.log("실행 결과");
+  for (let index = 0; index < attemptCount.value; index++) {
+    cars.forEach((it) => {
+      it.move(getRandomIntInclusive(0, 9));
+      console.log(renderCar(it));
+    });
+    console.log();
+  }
+}
+
+function renderCar(car) {
+  return `${car.name} : ${"-".repeat(car.position)}`;
+}
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
 }
