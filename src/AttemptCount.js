cacc1f46962f08fb14368628ee203687a5369f44
woowahan-pjs <jason@woowahan.com>
2024-01-08 16:53:41 +0900

    fix: modify the attempt count to allow only decimal numbers

--- src/AttemptCount.js
+++ src/AttemptCount.js
@@ -2,7 +2,7 @@ class AttemptCount {
   #value;
 
   constructor(value) {
-    value = parseInt(value);
+    value = parseInt(value, 10);
     if (!value || value < 0) {
       throw new Error("시도 횟수는 0보다 큰 숫자여야 합니다.");
     }
